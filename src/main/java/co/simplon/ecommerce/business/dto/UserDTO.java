83fc60bbc4f5254c883cb4b2d66906a289b6050c
Thomas6A <thomas.baudrin@outlook.fr>
2024-03-12 11:26:08 +0100

    feat: add UserDTO

--- src/main/java/co/simplon/ecommerce/business/dto/UserDTO.java
+++ src/main/java/co/simplon/ecommerce/business/dto/UserDTO.java
@@ -1,5 +1,47 @@
 package co.simplon.ecommerce.business.dto;
 
 public class UserDTO {
+	
+	private int id;
+	
+	private String username;
+	
+	private String user_password;
+	
+	private String user_role;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUser_password() {
+		return user_password;
+	}
+
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+
+	public String getUser_role() {
+		return user_role;
+	}
+
+	public void setUser_role(String user_role) {
+		this.user_role = user_role;
+	}
+	
+	
 
 }
