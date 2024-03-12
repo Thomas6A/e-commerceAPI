63a3cefc3189154020b0c6cc8fe5e87b6c52f1ce
Thomas6A <thomas.baudrin@outlook.fr>
2024-03-12 12:01:40 +0100

    feat: add user service

---
+++ src/main/java/co/simplon/ecommerce/business/service/user/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package co.simplon.ecommerce.business.service.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import co.simplon.ecommerce.business.convert.UserConvert;
+import co.simplon.ecommerce.business.dto.UserDTO;
+import co.simplon.ecommerce.persistance.repository.UserRepository;
+
+public class UserServiceImpl implements IUserService {
+	
+	private UserRepository repo;
+
+	@Override
+	public void createUser(UserDTO user) {
+		repo.save(UserConvert.getInstance().convertDTOToEntity(user));
+		
+	}
+
+	@Override
+	public void deleteUser(UserDTO user) {
+		repo.delete(UserConvert.getInstance().convertDTOToEntity(user));
+		
+	}
+
+	@Autowired
+	public void setRepo(UserRepository repo) {
+		this.repo = repo;
+	}
+	
+	
+
+}
