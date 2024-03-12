ecc29c29d1514fdff1206933e5219667ea54ca94
NicolasDrp <drapier.nicolas07@gmail.com>
2024-03-12 12:18:55 +0100

    feat(presentation): DeleteProductController

---
+++ src/main/java/co/simplon/ecommerce/presentation/controller/product/DeleteProductController.java
@@ -0,0 +1,28 @@
+package co.simplon.ecommerce.presentation.controller.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.simplon.ecommerce.business.dto.ProductDTO;
+import co.simplon.ecommerce.business.service.product.IProductService;
+
+@RestController
+@CrossOrigin
+public class DeleteProductController {
+
+	private IProductService service;
+
+	@GetMapping("/products")
+	public void deleteProduct(@RequestBody ProductDTO dto) {
+		service.deleteProduct(dto);
+	}
+
+	@Autowired
+	public void setService(IProductService service) {
+		this.service = service;
+	}
+
+}
