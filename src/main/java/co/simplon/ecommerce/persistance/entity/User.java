1a705bdecbc841a312349bf95f89cc9d35d693fa
Thomas6A <thomas.baudrin@outlook.fr>
2024-03-12 11:22:48 +0100

    feat: persistance user

--- src/main/java/co/simplon/ecommerce/persistance/entity/User.java
+++ src/main/java/co/simplon/ecommerce/persistance/entity/User.java
@@ -1,5 +1,61 @@
 package co.simplon.ecommerce.persistance.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "user")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private String username;
+	
+	@Column
+	private String user_password;
+	
+	@Column
+	private String user_role;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUser_password() {
+		return user_password;
+	}
+
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+
+	public String getUser_role() {
+		return user_role;
+	}
+
+	public void setUser_role(String user_role) {
+		this.user_role = user_role;
+	}
+	
+	
 
 }
