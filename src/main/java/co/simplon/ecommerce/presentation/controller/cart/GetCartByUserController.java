e2735a2bbc66ef3e0460daa78f84e17c648b03ed
Thomas6A <thomas.baudrin@outlook.fr>
2024-03-13 11:54:00 +0100

    feat: Controller get cart by user

---
+++ src/main/java/co/simplon/ecommerce/presentation/controller/cart/GetCartByUserController.java
@@ -0,0 +1,28 @@
+package co.simplon.ecommerce.presentation.controller.cart;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.simplon.ecommerce.business.dto.UserDTO;
+import co.simplon.ecommerce.business.service.cart.ICartService;
+
+@RestController
+@CrossOrigin
+public class GetCartByUserController {
+	
+private ICartService service;
+	
+	@GetMapping("/cart")
+	public void getCartByUser(@RequestBody UserDTO dto) {
+		service.getCartByUser(dto);
+	}
+	
+	@Autowired
+	public void setService(ICartService service) {
+		this.service = service;
+	}
+
+}
