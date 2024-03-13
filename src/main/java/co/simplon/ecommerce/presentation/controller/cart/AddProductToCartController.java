7af734cfd8c27dbc4ebd121b699fe23d585283d2
Thomas6A <thomas.baudrin@outlook.fr>
2024-03-13 11:59:40 +0100

    feat: add product to cart controller

---
+++ src/main/java/co/simplon/ecommerce/presentation/controller/cart/AddProductToCartController.java
@@ -0,0 +1,29 @@
+package co.simplon.ecommerce.presentation.controller.cart;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.simplon.ecommerce.business.dto.CartDTO;
+import co.simplon.ecommerce.business.dto.ProductDTO;
+import co.simplon.ecommerce.business.service.cart.ICartService;
+
+@RestController
+@CrossOrigin
+public class AddProductToCartController {
+	
+private ICartService service;
+	
+	@PutMapping("/cart")
+	public void addProductToCart(@RequestBody CartDTO cart, @RequestBody ProductDTO product) {
+		service.addProductToCart(cart, product);
+	}
+	
+	@Autowired
+	public void setService(ICartService service) {
+		this.service = service;
+	}
+
+}
